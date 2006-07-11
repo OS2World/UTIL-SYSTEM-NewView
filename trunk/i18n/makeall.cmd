8daaaadc59c1bdc5e14f5595189c0a27345eed6d
rbri <rbri@2f29dc4a-6615-0410-8a4c-3367f1248613>
2006-07-11 19:11:24 +0000

    + copy Changes.txt % fix dont copy .svn dir content

--- trunk/i18n/makeall.cmd
+++ trunk/i18n/makeall.cmd
@@ -20,23 +20,30 @@ nv_i18n_dir = directory()
 nv_build_dir = value('NV_BUILD', , 'OS2ENVIRONMENT')
 nv_rc = SysMkDir(nv_build_dir)
 
+/* copy changes.txt */
+copy Changes.txt nv_build_dir
+
+
 /* compile the ipf file */
 ipfc NewView.ipf nv_build_dir||'\NewView.hlp'
 
 call SysFileTree nv_i18n_dir||'\', subdirs, 'DO'
 do i=1 to subdirs.0
-    if 0 = lastpos('images', subdirs.i) then
+    if  0 = lastpos('images', subdirs.i) then
     do
-        nv_locale = substr(subdirs.i, lastpos('\',subdirs.i) + 1)
+        if  0 = lastpos('.svn', subdirs.i) then
+        do
+            nv_locale = substr(subdirs.i, lastpos('\',subdirs.i) + 1)
 
-        /* compile the ipf file inside the dir */
-        call SysFileTree subdirs.i || '\NewView_' || nv_locale || '.ipf', ipffiles, 'FO'
-        do j=1 to ipffiles.0
-            ipfc ipffiles.j nv_build_dir || '\NewView_' || nv_locale || '.hlp'
+            /* compile the ipf file inside the dir */
+            call SysFileTree subdirs.i || '\NewView_' || nv_locale || '.ipf', ipffiles, 'FO'
+            do j=1 to ipffiles.0
+                ipfc ipffiles.j nv_build_dir || '\NewView_' || nv_locale || '.hlp'
+            end
+
+            /* copy the lang file */
+            copy subdirs.i || '\newview_' || nv_locale || '.lng' nv_build_dir
         end
-
-        /* copy the lang file */
-        copy subdirs.i || '\newview_' || nv_locale || '.lng' nv_build_dir
     end
 end
 
