155e8a1fa3e77f8b65be48c71b5490abc42dda87
rbri <rbri@2f29dc4a-6615-0410-8a4c-3367f1248613>
2007-01-10 19:50:42 +0000

    + pmprintf for debug output

--- trunk/env.cmd
+++ trunk/env.cmd
@@ -25,9 +25,8 @@ REM Output Directory.
 REM Don't change this. There is no way to use this in the Sibyl project files.
 REM Please use a tool like NetDrive to ensure this path exists.
 REM
-SET NV_BUILD=P:\newview_dev\build
+SET NV_DEV=P:\newview_dev
 REM --------------------------------------------------------------------------
-
 
 REM --------------------------------------------------------------------------
 REM Enable debuging menus.
@@ -39,9 +38,13 @@ REM --------------------------------------------------------------------------
 REM Do not change one of the following lines
 REM
 
+SET NV_BUILD=%NV_DEV%\build
+SET PMPRINTF_PATH=%NV_DEV%\tools\pmprintf
+
 SET PATH=%WATCOM%\BINP;%WATCOM%\BINW;%PATH%
-SET INCLUDE=%WATCOM%\H;%WATCOM%\H\OS2;
+SET INCLUDE=%WATCOM%\H;%WATCOM%\H\OS2;%PMPRINTF_PATH%;
 SET HELP=%WATCOM%\BINP\HELP;%HELP%
 SET BOOKSHELF=%WATCOM%\BINP\HELP;%BOOKSHELF%
 SET BEGINLIBPATH=%WATCOM%\BINP\DLL
+
 
