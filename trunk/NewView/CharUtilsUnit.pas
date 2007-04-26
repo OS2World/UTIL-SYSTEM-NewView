66631d74743053c07d9d5f0c8e2221cdb72473a5
rbri <rbri@2f29dc4a-6615-0410-8a4c-3367f1248613>
2007-04-26 11:53:33 +0000

    Pchar utils added

--- trunk/NewView/CharUtilsUnit.pas
+++ trunk/NewView/CharUtilsUnit.pas
@@ -26,6 +26,19 @@ const
   // Returns true if aChar is an alphabetic character a..z A..Z
   Function CharIsAlpha(const aChar : char) : boolean;
 
+
+  // ---------------
+  // ---- PChar ----
+  // ---------------
+
+  
+  // Converts a PChar into a String like StrPas
+  // but conversts at least the first aLength chars
+  Function StrPasWithLength(const aPChar: PChar; const aLength: integer) : String;
+
+  // Returns the difference of the pointers
+  Function PCharPointerDiff(const aMinuend: PChar; const aSubtrahend : PChar ) : Longword;
+
 Implementation
 
   uses
@@ -39,6 +52,33 @@ Implementation
   Function CharIsAlpha(const aChar : char) : boolean;
   begin
     Result := ( (aChar >= 'A') and (aChar <= 'Z') ) or ((aChar >= 'a') and (aChar <= 'z'));
+  end;
+
+
+  // ---------------
+  // ---- PChar ----
+  // ---------------
+
+
+  // Converts a PChar into a String like StrPas
+  // but conversts at least the first aLength chars
+  Function StrPasWithLength(const aPChar: PChar; const aLength: integer) : String;
+  var
+    i: integer;
+  begin
+    Result := '';
+    i := 0;
+    while (aPChar[i] <> #0) and (i < aLength) do
+    begin
+      Result := Result + aPChar[i];
+      inc( i );
+    end;
+  end;
+
+
+  Function PCharPointerDiff(const aMinuend: PChar; const aSubtrahend : PChar ) : Longword;
+  begin
+    Result := Longword(aMinuend) - Longword(aSubtrahend);
   end;
 
 
