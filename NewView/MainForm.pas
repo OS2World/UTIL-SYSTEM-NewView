9d76d00ba1aa5e3236448e06630bd81eafd99831
rbri <rbri@2f29dc4a-6615-0410-8a4c-3367f1248613>
2007-04-29 18:22:22 +0000

    changed copyright

--- NewView/MainForm.pas
+++ NewView/MainForm.pas
@@ -1,7 +1,8 @@
 Unit MainForm;
 
 // NewView - a new OS/2 Help Viewer
-// Copyright 2003 Aaron Lawrence (aaronl at consultant dot com)
+// Copyright 2003-2006 Aaron Lawrence (aaronl at consultant dot com)
+// Copyright 2006-2007 Ronald Brill (rbri at rbri dot de)
 // This software is released under the Gnu Public License - see readme.txt
 
 Interface
