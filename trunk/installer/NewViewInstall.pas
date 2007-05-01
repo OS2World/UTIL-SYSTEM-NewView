5ab23847058246e90b2bf6cb9043c4c4ada989fb
rbri <rbri@2f29dc4a-6615-0410-8a4c-3367f1248613>
2007-05-01 12:06:18 +0000

    initial checkin

---
+++ trunk/installer/NewViewInstall.pas
@@ -0,0 +1,13 @@
+Program NewViewInstall;
+
+Uses
+  Forms, MainFormUnit, ChooseFolderFormUnit;
+
+{$r NewViewInstall.scu}
+
+Begin
+  Application.Create;
+  Application.CreateForm (TMainForm, MainForm);
+  Application.Run;
+  Application.Destroy;
+End.
